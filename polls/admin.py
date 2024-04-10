227f97f6f93c12fbb6d6fa9144dd415be3e2836e
gimtaeyeon <taeyeoon@naver.com>
2024-04-10 18:45:31 +0900

    중간 커밋

---
+++ polls/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Choice, Question
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('질문 섹션', {'fields': ['question_text']}),
+        ('생성일', {'fields': ['pub_date'], 'classes': ['collapse']}),        
+    ]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    readonly_fields = ['pub_date']
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields = ['question_text','choice__choice_text']
+
+
+admin.site.register(Question, QuestionAdmin)
---
+++ polls/views.py
@@ -0,0 +1,39 @@
+from .models import *
+from django.http import HttpResponse, HttpResponseRedirect
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.db.models import F
+
+def index(request) :
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'questions': latest_question_list} 
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    """
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {'question': question, 'error_message': f"선택이 없습니다. id={request.POST['choice']}"})
+    else:
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:result', args=(question.id,)))
+
+def result(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/result.html', {'question': question})
+
+def some_url(request) :
+    return HttpResponse("some_url을 구현해 봤습니다")
